20943f3f326e72ea7c5464bc2c3eee06703ed404
prabhathur <prabhathur@gmail.com>
2021-01-29 22:41:34 +0530

    removed print

--- dice_ml/explainer_interfaces/feasible_base_vae.py
+++ dice_ml/explainer_interfaces/feasible_base_vae.py
@@ -191,7 +191,7 @@ class FeasibleBaseVAE(ExplainerBase):
             for cf_count in range(total_CFs):                                
                 recon_err, kl_err, x_true, x_pred, cf_label = self.cf_vae.compute_elbo( train_x, 1.0-train_y, self.pred_model )
                 while( cf_label== train_y):
-                    print(cf_label, train_y)
+                    #print(cf_label, train_y)
                     recon_err, kl_err, x_true, x_pred, cf_label = self.cf_vae.compute_elbo( train_x, 1.0-train_y, self.pred_model )
                     
                 x_pred= x_pred.detach().numpy()
